a5f8acd1936347ccf57ab6facf5dc659ca59db52
Mubashar Hassan <mubasshar009@gmail.com>
2021-01-18 19:21:56 +0500

    Added all files

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import './Hello.css';
+import logo from './logo.svg';
+
+function Hello() {
+    return (
+        <div>
+            <h1 class = "Heading">Hello From Hello.js</h1>
+            <p class = "para">This is my first React Tutorial </p>
+        </div>
+    );
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    //<App name = "Mubashar Hassan" age = "26"/>,
+    <Hello />,
+  
   document.getElementById('root')
 );
 
